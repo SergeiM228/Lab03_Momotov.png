21b31f93bde2c98c3f27d3d5cc1b53a28899d667
SergeiM228 <mmomotov9@gmail.com>
2026-09-15 09:56:31 +0300

    Lab03: increment and decrement

--- Program.cs
+++ Program.cs
@@ -1,35 +1,46 @@
-﻿// Console.WriteLine("Банковский счет");
+﻿// // Console.WriteLine("Банковский счет");
 
-// double balance = 1000;
-// Console.WriteLine($"Начальный баланс: {balance}");
+// // double balance = 1000;
+// // Console.WriteLine($"Начальный баланс: {balance}");
 
-// balance += 500;
-// Console.WriteLine($"После пополнения на 500: {balance}");
+// // balance += 500;
+// // Console.WriteLine($"После пополнения на 500: {balance}");
 
-// balance -= 200;
-// Console.WriteLine($"После покупки на 200: {balance}");
+// // balance -= 200;
+// // Console.WriteLine($"После покупки на 200: {balance}");
 
-// balance *= 1.05;
-// Console.WriteLine($"После начисления 5%: {balance}");
+// // balance *= 1.05;
+// // Console.WriteLine($"После начисления 5%: {balance}");
 
-// balance /= 2;
-// Console.WriteLine($"После деления пополам:{balance}");
+// // balance /= 2;
+// // Console.WriteLine($"После деления пополам:{balance}");
+// Console.WriteLine();
+// Console.WriteLine("Постфикс vs префикс");
+
+// int lessonNumber = 1;
+// Console.WriteLine($"lessonNumber++  выводит: {lessonNumber++}");
+// Console.WriteLine($"После этого  lessonNumber =  {lessonNumber}");
+// int weekNumber = 1;
+// Console.WriteLine($"++weekNumber выводит: {++weekNumber}");
+// Console.WriteLine($"После этого weekNumber = {weekNumber}");
+
+// Console.WriteLine();
+// Console.WriteLine("Практическая ловушка");
+
+// int attempts = 0;
+// Console.WriteLine($"Попытка №{++attempts}");
+
+// Console.WriteLine($"Попытка №{++attempts}");
+// Console.WriteLine($"Попытка №{++attempts}");
+// Console.WriteLine($"Всего попыток: {attempts}");
 Console.WriteLine();
-Console.WriteLine("Постфикс vs префикс");
+Console.WriteLine("Операторы сравнения");
 
-int lessonNumber = 1;
-Console.WriteLine($"lessonNumber++  выводит: {lessonNumber++}");
-Console.WriteLine($"После этого  lessonNumber =  {lessonNumber}");
-int weekNumber = 1;
-Console.WriteLine($"++weekNumber выводит: {++weekNumber}");
-Console.WriteLine($"После этого weekNumber = {weekNumber}");
-
-Console.WriteLine();
-Console.WriteLine("Практическая ловушка");
-
-int attempts = 0;
-Console.WriteLine($"Попытка №{++attempts}");
-
-Console.WriteLine($"Попытка №{++attempts}");
-Console.WriteLine($"Попытка №{++attempts}");
-Console.WriteLine($"Всего попыток: {attempts}");
+double myGrade = 4.6;
+double passingGrade = 4.0;
+int myAge = 20;
+int votingAge = 18;
+bool isPassing = myGrade >= passingGrade;
+bool isExactAge = myAge == votingAge;
+bool canVote = myAge >= votingAge;
+bool isNotFailing = myGrade != 2.0;
